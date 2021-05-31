628a07279adbec74283c8054810e59c3c7fd8617
John <jeastcloud@gmail.com>
2021-05-31 05:26:55 -0500

    update

--- Vagrantfile
+++ Vagrantfile
@@ -1,20 +1,20 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# All Vagrant configuration is done below. The "" in Vagrant.configure
+# All Vagrant configuration is done below. The  in Vagrant.configure
 # configures the configuration version (we support older styles for
 # backwards compatibility). Please don't change it unless you know what
 # you're doing.
 Vagrant.configure('') do |config|
 
-    config.vm.define "droplet1" do |config|
+    config.vm.define droplet1 do |config|
         config.vm.provider :digital_ocean do |provider, override|
           override.ssh.private_key_path = '~/.ssh/id_rsa'
           override.vm.box = 'digital_ocean'
-          override.vm.box_url = "https://github.com/devopsgroup-io/vagrant-digitalocean/raw/master/box/digital_ocean.box"
+          override.vm.box_url = https://github.com/devopsgroup-io/vagrant-digitalocean/raw/master/box/digital_ocean.box
           override.nfs.functional = false
           override.vm.allowed_synced_folder_types = :rsync
-          provider.token = 'TOKEN'
+          provider.token = '02f10529a4cdc805de7ab09d5a2e423b7334d0821b2f6cae8b7903f000504469'
           provider.image = 'IMAGE'
           provider.region = 'REGION'
           provider.size = 'SIZE'
@@ -24,24 +24,8 @@ Vagrant.configure('') do |config|
           provider.monitoring = false
         end
     end
-  
-    config.vm.define "droplet2" do |config|
-  
-        config.vm.provider :digital_ocean do |provider, override|
-          override.ssh.private_key_path = '~/.ssh/id_rsa'
-          override.vm.box = 'digital_ocean'
-          override.vm.box_url = "https://github.com/devopsgroup-io/vagrant-digitalocean/raw/master/box/digital_ocean.box"
-          override.nfs.functional = false
-          override.vm.allowed_synced_folder_types = :rsync
-          provider.token = 'TOKEN'
-          provider.image = 'IMAGE'
-          provider.region = 'REGION'
-          provider.size = 'SIZE'
-          provider.backups_enabled = false
-          provider.private_networking = false
-          provider.ipv6 = false
-          provider.monitoring = false
-        end
-    end 
-  
+    
+    
+    "ADD1"
+
   end
--- vagrantscript.py
+++ vagrantscript.py
@@ -1,6 +1,51 @@
 import os
 import sys
+#### dont forget to replace Vagrantfile with Vagrantfile
 
+
+## Droplet number 1-4
+Add = "ADD1"
+
+
+Vagrant_file_add = """
+    config.vm.define 'NAME2' do |config|
+        config.vm.provider :digital_ocean do |provider, override|
+          override.ssh.private_key_path = '~/.ssh/id_rsa'
+          override.vm.box = 'digital_ocean'
+          override.vm.box_url = 'https://github.com/devopsgroup-io/vagrant-digitalocean/raw/master/box/digital_ocean.box'
+          override.nfs.functional = false
+          override.vm.allowed_synced_folder_types = :rsync
+          provider.token = 'TOKEN'
+          provider.image = 'IMAGE'
+          provider.region = 'REGION'
+          provider.size = 'SIZE'
+          provider.backups_enabled = false
+          provider.private_networking = false
+          provider.ipv6 = false
+          provider.monitoring = false
+        end
+    end"""
+striped = Vagrant_file_add.replace('"',"")
+
+
+droplet_number = int(input("How many Droplets do you want?[1, 2, 3 or 4]: "))
+if droplet_number == 1:
+    with open("Vagrantfile") as f:
+        addNEWdroplet=f.read().replace(Add , striped)
+
+
+    with open("Vagrantfile", "w") as f:
+        f.write(addNEWdroplet.replace('"', ""))
+
+## Droplet1 name change
+
+with open("Vagrantfile") as f:
+    droplet_1_Change=f.read().replace("NAME2", "droplet2")
+
+with open("Vagrantfile", "w") as f:
+    f.write(droplet_1_Change)
+
+## TOKEN
 
 digital_ocean_TOKEN = input("Paste token here: ")
 
@@ -9,6 +54,8 @@ with open("Vagrantfile") as f:
 
 with open("Vagrantfile", "w") as f:
     f.write(newToken)
+
+## Choosing Image
 
 cmd_run_vagrant_list_IMAGES = str("vagrant digitalocean-list images {}".format(digital_ocean_TOKEN))
 os.system(cmd_run_vagrant_list_IMAGES)
@@ -23,6 +70,7 @@ with open("Vagrantfile") as f:
 with open("Vagrantfile", "w") as f:
     f.write(newImage)
 
+## Choosing Region
 
 cmd_run_vagrant_list_REGIONS = str("vagrant digitalocean-list regions {}".format(digital_ocean_TOKEN))
 os.system(cmd_run_vagrant_list_REGIONS)
@@ -37,6 +85,8 @@ with open("Vagrantfile") as f:
 with open("Vagrantfile", "w") as f:
     f.write(newRegion)
 
+## Choosing Size
+
 cmd_run_vagrant_list_SIZES = str("vagrant digitalocean-list sizes {}".format(digital_ocean_TOKEN))
 os.system(cmd_run_vagrant_list_SIZES)
 print("\nPlease choose a size\n")
@@ -50,7 +100,31 @@ with open("Vagrantfile") as f:
 with open("Vagrantfile", "w") as f:
     f.write(newSize)
 
+## Deleting outpute file if it exists
+
 if os.path.exists("output"):
     os.remove("output")
 else:
     None
+
+## Change SSH Key location
+
+ssh_key_ans = input("Do you want to specify /.ssh location?[y/n]: ")
+if ssh_key_ans == str.casefold("y"):
+    ssh_key_loc = input("What is the path?: ")
+
+    with open("Vagrantfile") as f:
+        newSSH=f.read().replace("~/.ssh/id_rsa", "{}".format(ssh_key_loc))
+
+    with open("Vagrantfile", "w") as f:
+        f.write(newSSH)
+    
+else:
+    print("Leaving default location (~/.ssh/id_rsa)!")
+
+
+
+
+
+            
+
